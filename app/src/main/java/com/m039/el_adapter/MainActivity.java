5fa26f907587ebcf8b6767c70be11c3a66117369
Dmitry Mozgin <m0391n@gmail.com>
2016-05-31 22:59:51 +0300

    First init

---
+++ app/src/main/java/com/m039/el_adapter/MainActivity.java
@@ -0,0 +1,15 @@
+package com.m039.el_adapter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import el_adapter.m039.com.myapplication.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
