27e0f847e20cb6905a0910e9b9966e4be5322b6d
Dmitry Mozgin <m0391n@gmail.com>
2016-06-02 00:39:51 +0300

    Integration with zvooq2

--- library/src/main/java/com/m039/el_adapter/ViewBindingBuilder.java
+++ library/src/main/java/com/m039/el_adapter/ViewBindingBuilder.java
@@ -21,7 +21,7 @@ import android.view.View;
 /**
  * Created by m039 on 6/1/16.
  */
-/* package */ abstract class ViewBindingBuilder<V extends View> {
+public class ViewBindingBuilder<V extends View> {
 
     final protected BaseViewAdapter adapter;
     final protected int viewType;
