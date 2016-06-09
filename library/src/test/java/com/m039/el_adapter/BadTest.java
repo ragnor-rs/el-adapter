44403bca2bff0a30002ca7937243cff7411f09ad
Dmitry Mozgin <m0391n@gmail.com>
2016-06-09 17:52:53 +0300

    jack&jill
    gradle version
    
    Travis
    
    Travis
    
    Same build tools
    
    New build tools
    
    New build tools
    
    tools
    
    Extra
    
    Build tools
    
    Build tools
    
    Emulator
    
    Script
    
    Java
    
    Java
    
    Java
    
    Remove release
    
    Library only
    
    Fix test

---
+++ library/src/test/java/com/m039/el_adapter/BadTest.java
@@ -0,0 +1,16 @@
+package com.m039.el_adapter;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+/**
+ * Created by m039 on 6/9/16.
+ */
+public class BadTest {
+
+    @Test
+    public void bad() {
+        Assertions.assertThat(true).isTrue();
+    }
+
+}
